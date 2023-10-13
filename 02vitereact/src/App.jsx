1a1e6e726d10d5eb7b0a56fdcb4e3a7d7c410091
Ankit <ankitkumar81458@gmail.com>
2023-10-14 00:27:49 +0530

    React fragment

--- 02vitereact/src/App.jsx
+++ 02vitereact/src/App.jsx
@@ -1,7 +1,17 @@
 import Chai from "./chai";
 
+//? NOTE: only JS evaluated exressions can be written in {}, inside a JSX
+
 function App() {
-  return <Chai />;
+  const username = "chai aur code";
+  return (
+    //! fragment
+    <>
+      <Chai />
+      <h1>chai aur react | {username}</h1>
+      <p>test para</p>
+    </>
+  );
 }
 
 export default App;
