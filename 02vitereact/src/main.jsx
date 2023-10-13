5d804edbb2a8c55c894f8fc832acafc07b32bcd9
Ankit <ankitkumar81458@gmail.com>
2023-10-14 00:28:21 +0530

    functional component, JSX and react.createElement

--- 02vitereact/src/main.jsx
+++ 02vitereact/src/main.jsx
@@ -2,8 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/*
+  !Functional component | normal js function
+  function MyApp() {
+    return (
+      <div>
+        <h1>This is my app!</h1>;
+      </div>
+    );
+  }
+  ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
+*/
+
+/*
+  !JSX | converts to object
+  const anotherElement = (
+    <a href="https://google.com" target="_blank">
+      Visit google
+    </a>
+  );
+  ReactDOM.createRoot(document.getElementById("root")).render(anotherElement);
+*/
+
+/*
+ !JSX internally gets converted to object using React.createElement()
+  const reactElement = React.createElement(
+    "a",
+    { href: "https://google.com", target: "_blank" },
+    "using react create element"
+  );
+  ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
+*/
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
